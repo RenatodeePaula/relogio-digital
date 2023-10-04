a32667a284136920c7bcb9cf103dad4c71d14d2e
Renato de Paula <renatodepaula.tec@hotmail.com>
2023-10-04 20:12:04 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+const getHours = document.querySelector(".hours")
+const getMinuts = document.querySelector(".minutes")
+const getSeconds = document.querySelector(".seconds")
+
+function updateTime() {
+    const now = new Date()
+    const hours = now.getHours().toString().padStart(2, "0")
+    const minutes = now.getMinutes().toString().padStart(2, "0")
+    const seconds = now.getSeconds().toString().padStart(2, "0")    
+          
+    getHours.textContent = hours
+    getMinuts.textContent = minutes
+    getSeconds.textContent = seconds 
+}
+setInterval(updateTime, 1000)
